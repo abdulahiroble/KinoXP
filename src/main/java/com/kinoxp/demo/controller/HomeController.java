63ed20dbe014b30d1a8401d7487ef0ec5f8d12e6
Thomas West <thom8750@stud.kea.dk>
2021-03-16 12:11:30 +0100

    HomeController added

--- src/main/java/com/kinoxp/demo/controller/HomeController.java
+++ src/main/java/com/kinoxp/demo/controller/HomeController.java
@@ -1,12 +1,13 @@
 package com.kinoxp.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class MovieController {
+public class HomeController {
 
+    @GetMapping("/")
+    public String getHomepage() {
 
+        return "index";
     }
-
+}
