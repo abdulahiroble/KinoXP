9493f0ae451007beef86de6fd04c09ac463cd62d
abdulahi roble <abdulahi_2610@hotmail.com>
2021-03-17 12:05:15 +0100

    Fix css

--- src/main/java/com/kinoxp/demo/config/SecurityConfiguration.java
+++ src/main/java/com/kinoxp/demo/config/SecurityConfiguration.java
@@ -28,10 +28,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+        .antMatchers( "/css/**").permitAll()
+        .antMatchers( "/img/**").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/").permitAll()
-                .antMatchers("/resources/**").permitAll().anyRequest().permitAll()
+                .antMatchers("/resources/**").permitAll()
                 .anyRequest().authenticated()
                 .and().formLogin().loginPage("/login").permitAll()
         ;
