220563c6cd918d25a5cf6a52d9f0d29b809f79b7
Thomas West <thom8750@stud.kea.dk>
2021-03-19 13:35:21 +0100

    RYTTED I KLASSER

--- src/main/java/com/kinoxp/demo/repositories/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kinoxp.demo.repositories;
-
-import com.kinoxp.demo.model.Employee;
-import com.kinoxp.demo.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
