ff8825cca061886aebc4ef6a902f4f3c14c4fd8d
abdulahi roble <abdulahi_2610@hotmail.com>
2021-03-23 10:34:57 +0100

    Fetch movies

--- src/main/java/com/kinoxp/demo/controller/MovieRestController.java
+++ src/main/java/com/kinoxp/demo/controller/MovieRestController.java
@@ -1,10 +1,13 @@
 package com.kinoxp.demo.controller;
 
+import java.util.List;
+
 import com.kinoxp.demo.model.Movie;
 import com.kinoxp.demo.repositories.MovieRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,14 +26,6 @@ public class MovieRestController {
     @ResponseStatus(HttpStatus.CREATED)
     public Movie newmovie(@ModelAttribute("movie") Movie movie) {
 
-        // try {
-        // userRepository.save(user);
-        // } catch (Exception e) {
-        // System.out.println("Error can't save to database " + e);
-        // }
-
-        // return "redirect:/register_success";
-
         System.out.println(movie);
         return movieRepository.save(movie);
     }
@@ -40,6 +35,11 @@ public class MovieRestController {
     public Movie newmoviejs(@RequestBody Movie movie) {
         System.out.println(movie);
         return movieRepository.save(movie);
+    }
+
+    @GetMapping("/movies")
+    public List<Movie> findAllMovies() {
+        return movieRepository.findAll();
     }
 
 }
