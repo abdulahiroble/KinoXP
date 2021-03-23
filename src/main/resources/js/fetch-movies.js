bfc60d8fc5cd403911a3cfb2d858b1890cd521a2
abdulahi roble <abdulahi_2610@hotmail.com>
2021-03-23 12:32:53 +0100

    Liste

---
+++ src/main/resources/js/fetch-movies.js
@@ -0,0 +1,37 @@
+let dropdown = document.getElementById('locality-dropdown');
+dropdown.length = 0;
+
+let defaultOption = document.createElement('option');
+defaultOption.text = 'Vælg film';
+
+dropdown.add(defaultOption);
+dropdown.selectedIndex = 0;
+
+const url = 'http://localhost:8080/movies';
+
+fetch(url)
+    .then(
+        function (response) {
+            if (response.status !== 200) {
+                console.warn('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response  
+            response.json().then(function (data) {
+                let option;
+
+                console.log(data);
+
+                for (let i = 0; i < data.length; i++) {
+                    option = document.createElement('option');
+                    option.text = data[i].title;
+                    dropdown.add(option);
+                }
+            });
+        }
+    )
+    .catch(function (err) {
+        console.error('Fetch Error -', err);
+    });
