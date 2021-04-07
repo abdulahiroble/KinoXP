37e8709ceb9f2442037d045c39859b2ccb4cf076
abdulahi roble <abdulahi_2610@hotmail.com>
2021-04-07 18:33:59 +0200

    Fetch movie and show in date

--- src/main/resources/js/moviecalendar.js
+++ src/main/resources/js/moviecalendar.js
@@ -1,7 +1,64 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
+
+const url = 'http://localhost:8080/movies';
+
+fetch(url)
+    .then(
+        function (response) {
+            if (response.status !== 200) {
+                console.warn('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
+
+            // Examine the text in the response  
+            response.json().then(function (data) {
+
+
+                var calendarEl = document.getElementById('calendar');
+
+                for (let key in data) {
+
+                    var calendar = new FullCalendar.Calendar(calendarEl, {
+                        initialView: 'dayGridMonth',
+                        headerToolbar: {
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        },
+                        events: [
+                            {
+                                title: data[key].title,
+                                start: '2021-04-12T10:30:00'
+                            },
+                            {
+                                title: data[key].title,
+                                start: '2021-04-12T12:00:00'
+                            },
+                            {
+                                title: data[key].title,
+                                start: '2021-04-12T14:30:00'
+                            },
+                            {
+                                title: data[key].title,
+                                start: '2021-04-13T07:00:00'
+                            }
+                        ]
+                    });
+
+
+                }
+
+                calendar.render();
+
+
+            });
+        }
+    )
+    .catch(function (err) {
+        console.error('Fetch Error -', err);
     });
-    calendar.render();
-});
+
+
+
+
+
